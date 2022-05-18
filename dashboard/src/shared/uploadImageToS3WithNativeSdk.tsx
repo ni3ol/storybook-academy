ba7ebbfd803c8f321b15acf2b67c1325c552cb02
Nicol Vojacek <nvojacek@gmail.com>
2022-05-18 16:39:13 +0200

    S3 integration

---
+++ dashboard/src/shared/uploadImageToS3WithNativeSdk.tsx
@@ -0,0 +1,52 @@
+import React, {useState} from 'react'
+import AWS from 'aws-sdk'
+
+const S3_BUCKET = 'storybook-library'
+const REGION = 'us-west-2'
+
+AWS.config.update({
+  accessKeyId: 'AKIAXWFILWYEWVLWBMU4',
+  secretAccessKey: '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',
+})
+
+const myBucket = new AWS.S3({
+  params: {Bucket: S3_BUCKET},
+  region: REGION,
+})
+
+const UploadImageToS3WithNativeSdk = () => {
+  const [progress, setProgress] = useState(0)
+  const [selectedFile, setSelectedFile] = useState(null)
+
+  const handleFileInput = (e: any) => {
+    setSelectedFile(e.target.files[0])
+  }
+
+  const uploadFile = (file: any) => {
+    const params = {
+      ACL: 'public-read',
+      Body: file,
+      Bucket: S3_BUCKET,
+      Key: file.name,
+    }
+
+    myBucket
+      .putObject(params)
+      .on('httpUploadProgress', (evt) => {
+        setProgress(Math.round((evt.loaded / evt.total) * 100))
+      })
+      .send((err) => {
+        if (err) console.log(err)
+      })
+  }
+
+  return (
+    <div>
+      <div>Native SDK File Upload Progress is {progress}%</div>
+      <input type="file" onChange={handleFileInput} />
+      <button onClick={() => uploadFile(selectedFile)}> Upload to S3</button>
+    </div>
+  )
+}
+
+export default UploadImageToS3WithNativeSdk
---
+++ dashboard/src/shared/uploadImageToS3WithReactS3.tsx
@@ -0,0 +1,38 @@
+import React, {useState} from 'react'
+import {uploadFile} from 'react-s3'
+
+const S3_BUCKET = 'storybook-library'
+const REGION = 'us-west-2'
+const ACCESS_KEY = 'AKIAXWFILWYEWVLWBMU4'
+const SECRET_ACCESS_KEY = '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'
+
+const config = {
+  bucketName: S3_BUCKET,
+  region: REGION,
+  accessKeyId: ACCESS_KEY,
+  secretAccessKey: SECRET_ACCESS_KEY,
+}
+
+const UploadImageToS3WithReactS3 = () => {
+  const [selectedFile, setSelectedFile] = useState(null)
+
+  const handleFileInput = (e: any) => {
+    setSelectedFile(e.target.files[0])
+  }
+
+  const handleUpload = async (file: any) => {
+    uploadFile(file, config)
+      .then((data: any) => console.log(data))
+      .catch((err: any) => console.error(err))
+  }
+
+  return (
+    <div>
+      <div>React S3 File Upload</div>
+      <input type="file" onChange={handleFileInput} />
+      <button onClick={() => handleUpload(selectedFile)}> Upload to S3</button>
+    </div>
+  )
+}
+
+export default UploadImageToS3WithReactS3
